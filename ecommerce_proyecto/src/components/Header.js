bd037a4746200326df1460ce1e68d56be7b61a55
Abigail Rosales <abbs18rosales@gmail.com>
2022-03-14 21:48:41 -0600

    Se crea componente Header

---
+++ ecommerce_proyecto/src/components/Header.js
@@ -0,0 +1,24 @@
+import React from "react";
+import '../Header.css';
+import logo from '../imgs/logoP.png'
+
+const Header = () => {
+    return(
+        <div>
+            <div className="logo">
+                <img src={logo} alt="" />
+            </div>        
+
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Galery</a></li>
+                    <li><a href="#">Shop</a></li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default Header;
